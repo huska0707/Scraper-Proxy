2eb3fecb94689407184ef96557dfdf24a426deb7
huska0707 <hussaink001haled@gmail.com>
2024-07-21 16:41:37 -0500

    immplment proxy scraper and testing methods

---
+++ index.js
@@ -0,0 +1,48 @@
+import os from 'os'
+import scrapers from './src/scrapers'
+
+const TYPES = ['http', 'socks']
+const VALID_TYPES = ['socks', 'socks5', 'socks4', 'https', 'http']
+
+export default class ProxyScraper {
+	constructor({ workerCount = os.cpus().length } = {}) {
+		this._workers = []
+		for (let i = 0; i < workerCount; i++) {
+			log('Spawning worker %d', i)
+			const worker = child.fork(path.join(__dirname, './src/worker.js'), [i])
+			worker.on('error', error => console.error(error))
+			this._workers.push(new Lock(worker))
+		}
+	}
+
+    getProxies(timeout) {
+        return this.scrapProxies().then(proxies => this.testProxies(timeout, proxies))
+    }
+
+	scrapProxies() {
+		const proxies = []
+		log('Scrapers: %o', Object.keys(ProxyScraper.scrapers))
+		for (let scraper in ProxyScraper.scrapers) {
+			proxies.push(
+				scrapers
+					[scraper]()
+					.then((proxies = []) => {
+						log('Found %d proxies from %s', proxies.length, scraper)
+						return proxies
+							.map(proxy => this._aggregateProxy(proxy, scraper))
+							.reduce((prev, next) => prev.concat(next), [])
+					})
+					.catch(e => {
+						log('Error while scraping proxies with %s\n%o', scraper, e)
+						return []
+					})
+			)
+		}
+		return Promise.all(proxies).then(values =>
+			values.reduce((prev, next) => prev.concat(next))
+		)
+	}
+
+}
+
+ProxyScraper.scrapers = scrapers
