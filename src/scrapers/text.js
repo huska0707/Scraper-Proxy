29bc8583e2debad46654a4fed658af5f7578eec0
huska0707 <hussaink001haled@gmail.com>
2024-07-21 20:13:38 -0500

    add proxy extraction function with IP and port validation

--- src/scrapers/text.js
+++ src/scrapers/text.js
@@ -7,3 +7,23 @@ export default function createTextScaper(url, aggregator) {
             .then(text => extractProxied(text, aggregator))
     }
 }
+
+
+export function extractProxies(text, aggregator = function() {}) {
+	return text
+		.split('\n')
+		.map(proxy => {
+			const more = proxy.split(' ')
+			const [ip, p] = more[0].split(':')
+			const port = parseInt(p)
+			if (validIp(ip) && validPort(port)) {
+				const agg = aggregator(more.slice(1))
+				return {
+					ip,
+					port,
+					...agg
+				}
+			}
+		})
+		.filter(proxy => proxy !== undefined)
+}
