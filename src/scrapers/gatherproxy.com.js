33e417023a38886e650e8022de813a0313161856
huska0707 <hussaink001haled@gmail.com>
2024-07-21 19:57:35 -0500

    enhance proxy scraping with session handling and anonymity levels

--- src/scrapers/gatherproxy.com.js
+++ src/scrapers/gatherproxy.com.js
@@ -1,4 +1,6 @@
 import debug from 'debug'
+import url from 'url'
+import fs from 'fs'
 
 const log = debug('proxy-scraper:gatherproxy.com')
 
@@ -17,4 +19,28 @@ const CAPTCHA_REPLACE = {
 	seven: 7,
 	eight: 8,
 	nine: 9
+}
+
+export default function scrap() {
+	const session = new Session(nodeFetch)
+	const fetch = (url, options) => session.fetch(url, options)
+	return getAccount(fetch).then(() => {
+		let chain = Promise.resolve([])
+		for (let level of ANONIMITY_LEVELS) {
+			chain = chain.then(data =>
+				getProxyListId(fetch, level)
+					.then(id => downloadProxyList(fetch, level, id))
+					.then(text =>
+						data.push(
+							extractProxies(text, () => ({
+								anonimity: ANONIMITY_LEVELS.indexOf(level),
+								type: 'http'
+							}))
+						)
+					)
+					.then(() => data)
+			)
+		}
+		return chain.then(datas => datas.reduce((prev, next) => prev.concat(next)))
+	})
 }
