837db8338f7b18f0f9f17d20d0af53e45e0a82ea
huska0707 <hussaink001haled@gmail.com>
2024-07-22 07:39:49 -0500

    error fixed in hidemy.name

--- src/scrapers/hidemy.name.js
+++ src/scrapers/hidemy.name.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch'
-
 import cheerio from '../util/fetch-cheerio'
 import { validIp, validPort } from './text'
 
 export default function scrap() {
 	const proxies = []
+	
 	for (let i = 0; i < 20; i++) {
 		proxies.push(
 			fetch(`https://hidemy.name/en/proxy-list/?start=${i * 64}`, {
@@ -20,7 +20,7 @@ export default function scrap() {
 							const ip = element.eq(0).text()
 							const port = element.eq(1).text()
 							const type = element.eq(4).text().split(',')[0].toLowerCase()
-							if (validIp(ip) && validPort(port)) return { ip, port}
+							if (validIp(ip) && validPort(port)) return { ip, port, type}
 						})
 						.filter(e => e !== undefined)
 						.get()
