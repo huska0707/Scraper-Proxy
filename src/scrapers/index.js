95f6a5fc7ee99d78608c1e5545d29a65d013e2c1
huska0707 <hussaink001haled@gmail.com>
2024-07-22 07:56:24 -0500

    NordVPN proxy scraper

--- src/scrapers/index.js
+++ src/scrapers/index.js
@@ -3,8 +3,10 @@ import gatherproxy from './gatherproxy.com'
 import hidester from './hidester.com'
 import hidemyname from './hidemy.name'
 import hidemyip from './hide-my-ip.com'
+import nordvpn from './nordvpn.com'
 import multiproxy from './multiproxy.org'
 import premproxy from './premproxy.com'
+import proxydb from './proxydb.net'
 
 export default {
     'free-proxy-list.net': freeproxylist,
@@ -14,4 +16,6 @@ export default {
     'hidester.com': hidester,
     'multiproxy.org': multiproxy,
     'premproxy.com': premproxy,
+    'nordvpn.com': nordvpn,
+    'proxydb.net': proxydb,
 }
