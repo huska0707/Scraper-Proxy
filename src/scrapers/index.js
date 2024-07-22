3237e62fab319048fc24b0d10040f30ddcc09b4f
huska0707 <hussaink001haled@gmail.com>
2024-07-22 07:37:09 -0500

    update proxy scraper to use Hidester API

--- src/scrapers/index.js
+++ src/scrapers/index.js
@@ -1,9 +1,11 @@
 import freeproxylist from './free-proxy-list.net'
 import gatherproxy from './gatherproxy.com'
+import hidester from './hidester.com'
 import hidemyip from './hide-my-ip.com'
 
 export default {
     'free-proxy-list.net': freeproxylist,
     'gatherproxy.com': gatherproxy,
     'hide-my-ip.com': hidemyip,
+    'hidester.com': hidester,
 }
