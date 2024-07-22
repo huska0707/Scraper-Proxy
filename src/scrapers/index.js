c954bb61b1942dcad99044da12fd5c9a8453729d
huska0707 <hussaink001haled@gmail.com>
2024-07-22 07:35:22 -0500

    proxy scraping functionality with anonymity levels

--- src/scrapers/index.js
+++ src/scrapers/index.js
@@ -1,7 +1,9 @@
 import freeproxylist from './free-proxy-list.net'
 import gatherproxy from './gatherproxy.com'
+import hidemyip from './hide-my-ip.com'
 
 export default {
     'free-proxy-list.net': freeproxylist,
     'gatherproxy.com': gatherproxy,
+    'hide-my-ip.com': hidemyip,
 }
